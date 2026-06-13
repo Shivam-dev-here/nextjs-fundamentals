3b0188b78ef54cfb696c5c6b09184dc46eef47f9
Shivam <mypersonalishere.mypersonal@gmail.com>
2026-06-13 21:11:16 +0530

    feat: useSearchParams

--- 10-navigation/my-app/app/page.js
+++ 10-navigation/my-app/app/page.js
@@ -1,12 +1,18 @@
+"use client"
+import { useSearchParams } from "next/navigation";
 import Image from "next/image";
 
 export default function Home() {
+  const searchParams = useSearchParams();
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
    
-   Home | Page
+   Home | Page | blog is {searchParams.get('blog')} and {searchParams.get('utm_source')}
 
    <div>Shivam-dev-here</div>
+   <div>
+
+   </div>
     </div>
   );
 }
